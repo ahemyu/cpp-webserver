94659f09c9c8d7b24e46f946260871a87e33d7ed
ahemyu <hansovic7@gmail.com>
2026-03-10 16:11:37 +0100

    feat: bind socket to localhost and 8080 port

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <cstdio>
 #include <sys/socket.h>
 #include <sys/types.h>
+ #include <netinet/in.h>
 
 typedef char unsigned u8;
 typedef short unsigned u16;
@@ -17,6 +18,20 @@ int main(){
     int socketCode  = socket(AF_INET, SOCK_STREAM, 0);
     if(socketCode < 0){
       printf("Error opening Socket! \n");
+      return 1;
+    }
+    sockaddr_in adr{}; //TODO: set this so that it is some local ip
+    adr.sin_family = AF_INET;
+    adr.sin_port = 0x901F; //reverse notation for 8080 in hex bc x68 machines they store it Little Endian but we need network byte order (Big Endian)
+    in_addr localhost;
+    localhost.s_addr = 0x0100007F; //reverse notation for 127.0.0.1 in hex bc x68 machines they store it Little Endian but we need network byte order (Big Endian)
+    adr.sin_addr = localhost;
+    socklen_t len = sizeof(adr);
+
+    s32 bindReturn = bind(socketCode, reinterpret_cast<const sockaddr*>(&adr), len);
+    if(bindReturn != 0){
+      printf("Error binding Socket! \n");
+      return 1;
     }
 
   return 0;
